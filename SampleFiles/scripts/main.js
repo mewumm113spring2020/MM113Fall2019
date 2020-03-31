fd21d677a04d9f05e7487fa407abb05b0b112b58
mm113 <meucsf2020>
2020-03-30 22:49:14 -0700

    Answer Q1, Q2, Q3, Q4

--- SampleFiles/scripts/main.js
+++ SampleFiles/scripts/main.js
@@ -11,6 +11,9 @@ const modifyText = () => {
   //Change the text of user2 to be the handle from the StarTrekTweet object
   //Changes the text of message 2 class to be the handle from the StarTrekTweet object
   // YOUR CODE BELOW THIS LINE.
+  document.getElementById('user2').innerHTML = StarTrekTweet.name;
+  document.getElementById('link2').innerHTML = StarTrekTweet.handle;
+  document.getElementById('message2').innerHTML = StarTrekTweet.text;
 }
 
 const changeLinkAndImageAttributes = () => {
@@ -18,6 +21,9 @@ const changeLinkAndImageAttributes = () => {
   //Sets the href attribute for link2 to point to the second element in the links array
   //Sets the src attribute for the Ohlone image to point to the third element in the links array
   // YOUR CODE BELOW THIS LINE.
+  document.getElementById('link1').setAttribute("href", links[0]);
+  document.getElementById('link2').setAttribute("href", links[1]);
+  document.getElementById('Ohlone').setAttribute("src", links[2]);
 }
 
 const changeClassAttribute = () => {
@@ -25,6 +31,22 @@ const changeClassAttribute = () => {
   //function so that it replaces the “tweet” class with the “tweet-pink” class for the two tweets.
   // change id="tweet1" class="tweet" to id="tweet1" class="tweet-pink"
   // YOUR CODE BELOW THIS LINE
+
+  // approach #1
+  // while (document.getElementsByClassName("tweet").length > 0) {
+  //   document.getElementsByClassName("tweet")[0].setAttribute("class", "tweet-pink");
+  // }
+  
+  // approach #2
+  // while (document.querySelectorAll(".tweet").length > 0) {
+  //   document.querySelectorAll(".tweet")[0].setAttribute("class", "tweet-pink");
+  // }
+
+  var elem = document.querySelector("#tweet1");
+  elem.setAttribute("class", "tweet-pink");
+  elem = document.querySelector("#tweet2");
+  elem.setAttribute("class", "tweet-pink");
+
   
 }
 
@@ -35,5 +57,13 @@ const appendElements = () => {
   //points to an image of your choice,symbolizing something about the course.
   //id="tweet1"
   // YOUR CODE BELOW THIS LINE
+  var tweet1 = document.getElementById("tweet1");
+  var new_link = document.createElement("a");
+  new_link.textContent = "Intro to JavaScript.";
+  new_link.setAttribute("href","http://www.ohlone.edu");
+  tweet1.appendChild(new_link);
+  var new_img = document.createElement("img");
+  new_img.setAttribute("src","https://developers.google.com/web/updates/images/2014-12-02-fundamentals-of-web-dev/image03.gif")
+  tweet1.appendChild(new_img);
 
 }
